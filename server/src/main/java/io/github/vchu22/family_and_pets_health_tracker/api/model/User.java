328be0c2302a2bf2adc124d5f12c707ded3a677f
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-07-29 20:20:05 -0400

    set up User model

--- server/src/main/java/io/github/vchu22/family_and_pets_health_tracker/api/model/User.java
+++ server/src/main/java/io/github/vchu22/family_and_pets_health_tracker/api/model/User.java
@@ -1,4 +1,43 @@
 package io.github.vchu22.family_and_pets_health_tracker.api.model;
 
 public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(int id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
