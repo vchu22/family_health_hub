2fe220c3903ac809da19d0d8935b353e141ccc25
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-07-29 21:01:09 -0400

    created controller and service for getting user information

--- server/src/main/java/io/github/vchu22/family_and_pets_health_tracker/api/controller/UserController.java
+++ server/src/main/java/io/github/vchu22/family_and_pets_health_tracker/api/controller/UserController.java
@@ -1,4 +1,27 @@
 package io.github.vchu22.family_and_pets_health_tracker.api.controller;
 
+import io.github.vchu22.family_and_pets_health_tracker.api.model.User;
+import io.github.vchu22.family_and_pets_health_tracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
 public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id) {
+        Optional user = userService.getUser(id);
+        if (user.isPresent()) {
+            return (User) user.get();
+        }
+        return null;
+    }
 }
