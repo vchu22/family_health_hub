3c9568a338751174cf9534f1ca18b26cb00a8aa5
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-06-13 15:33:06 -0400

    add methods to Member model class

--- server/server/main/models.py
+++ server/server/main/models.py
@@ -1,6 +1,9 @@
 from abc import ABC
 from django.db import models
 from django.contrib.auth.models import User  # Importing the built-in User model
+from datetime import date
+
+preset_pictures = ["", ""]
 
 class Household(models.Model):
     name = models.CharField(max_length=64)
@@ -14,13 +17,22 @@ class Member(models.Model, ABC):
     sex = models.CharField(max_length=1, choices=[('M','Male'),('F','Female'), ('T', 'Trans'), ('O', 'Other')], blank=True)
     picture = models.URLField(blank=True)
 
-class Person(Member):
-    admin = models.BooleanField(default=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='member_profile', null=True, blank=True)
-
-class Pet(Member):
-    name = models.CharField(max_length=50)
-    species = models.CharField(max_length=15)
-    breed = models.CharField(max_length=30, blank=True)
+    def set_template_picture(self, idx: int):
+        self.picture = preset_pictures[idx]
+        self.save()
+    
+    def calculate_age(self):
+        today = date.today()
+        age = today.year - self.birthdate.year - ((today.month, today.day) < (self.birthdate.month, self.birthdate.day))
+        return age
+    
     def __str__(self):
         return self.name
+
+class Person(Member):
+    admin = models.BooleanField(default=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='member_profile', null=True, blank=True) # link to an account
+
+class Pet(Member):
+    species = models.CharField(max_length=15)
+    breed = models.CharField(max_length=30, blank=True)
