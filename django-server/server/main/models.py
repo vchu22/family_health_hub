68cc24dfd27aeca446b6dc10cdf8b00e2b7c8a82
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-08-03 10:28:38 -0400

    feat: use updated method to make Member a abstract class, update gender/sex fields for persons and pets

--- django-server/server/main/models.py
+++ django-server/server/main/models.py
@@ -1,8 +1,8 @@
-from abc import ABC
 from django.db import models
-from django.contrib.auth.models import User  # Importing the built-in User model
+from django.contrib.auth import get_user_model
 from datetime import date
 
+User = get_user_model()
 preset_pictures = ["", ""]
 
 class Household(models.Model):
@@ -10,20 +10,22 @@ class Household(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 # abstract class
-class Member(models.Model, ABC):
+class Member(models.Model):
     household = models.ForeignKey(Household, related_name='members', on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
-    dob = models.DateField
-    sex = models.CharField(max_length=1, choices=[('M','Male'),('F','Female'), ('T', 'Trans'), ('O', 'Other')], blank=True)
+    dob = models.DateField()
     picture = models.URLField(blank=True)
 
+    class Meta:
+        abstract = True
+    
     def set_template_picture(self, idx: int):
         self.picture = preset_pictures[idx]
         self.save()
     
     def calculate_age(self):
         today = date.today()
-        age = today.year - self.birthdate.year - ((today.month, today.day) < (self.birthdate.month, self.birthdate.day))
+        age = today.year - self.dob.year - ((today.month, today.day) < (self.dob.month, self.dob.day))
         return age
     
     def __str__(self):
@@ -31,8 +33,33 @@ class Member(models.Model, ABC):
 
 class Person(Member):
     admin = models.BooleanField(default=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='member_profile', null=True, blank=True) # link to an account
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='member_profile', null=True, blank=True)
+    
+    # More nuanced gender/sex options for people
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('MF', 'Male-to-Female Transgender'),
+        ('FM', 'Female-to-Male Transgender'),
+        ('NB', 'Non-Binary'),
+        ('GF', 'Gender Fluid'),
+        ('O', 'Other'),
+        ('P', 'Prefer not to say'),
+    ]
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES, blank=True)
 
 class Pet(Member):
     species = models.CharField(max_length=15)
     breed = models.CharField(max_length=30, blank=True)
+    
+    # Biological sex for pets
+    SEX_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('NM', 'Neutered Male'),
+        ('SF', 'Spayed Female'),
+        ('I', 'Intersex'),
+        ('U', 'Unknown'),
+        ('NA', 'Not Applicable'),
+    ]
+    sex = models.CharField(max_length=2, choices=SEX_CHOICES, blank=True)
